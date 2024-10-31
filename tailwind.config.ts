99508ceff53216d388d00a111792453be5a90162
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2024-10-31 16:07:21 -0300

    added colos and anims

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,32 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      backgroundImage: {
+        "halloween-gradient":
+          "linear-gradient(135deg, #0d0b1a, #2d0b1a 40%, #531324 70%, #b84a0b)",
+      },
+      animation: {
+        "slow-pulse": "slow-pulse 4s linear infinite",
+        fadeOut: "fadeOut 0.5s ease-out forwards",
+      },
+      keyframes: {
+        "slow-pulse": {
+          "0%, 100%": { opacity: "1" },
+          "50%": { opacity: "0.7" },
+        },
+        fadeOut: {
+          "0%": { opacity: "0" },
+          "50%": { opacity: "0.5", scale: "2",color:"#2d0b1a" },
+          "99%": { opacity: "0", scale: "2" },
+          "100%": { opacity: "0", scale: "0.1" },
+        },
+      },
+      fontFamily: {
+        cinzel: ["var(--font-cinzel)", "serif"],
+        unifraktur: ["var(--font-unifraktur)", "serif"],
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
